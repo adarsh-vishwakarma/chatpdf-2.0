6ccd9bd5038745aecea3de79efaeb0a11c48ea6a
adarsh-vishwakarma <adarshvishwakarma704@gmail.com>
2024-11-07 08:24:25 +0530

    React Query Set Up

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -1,44 +1,15 @@
+import { messages } from "@/lib/db/schema";
 import { NextResponse } from "next/server";
-import { loadS3IntoPinecone } from "@/lib/pinecone";
-import { auth } from "@clerk/nextjs/server";
-import { error } from "console";
-import { db } from "@/lib/db";
-import { chats } from "@/lib/db/schema";
-import { getS3Url } from "@/lib/s3";
-
-
 
 // /api/create-chat
 export async function POST(req: Request, res: Response) {
-  const {userId} = await auth()
-  if(!userId) {
-    return NextResponse.json({error: "unauthorized"}, {status: 401})
-  }
   try {
     const body = await req.json();
-    const { file_key, file_name} = body;
-    console.log(file_key, file_name)
-    const pages = await loadS3IntoPinecone(file_key)
-    const chat_id = await db
-      .insert(chats)
-      .values({
-        fileKey: file_key,
-        pdfName: file_name,
-        pdfUrl: getS3Url(file_key),
-        userId,
-      })
-      .returning({
-        insertedId: chats.id,
-      });
-
-    return NextResponse.json(
-      {
-        chat_id: chat_id[0].insertedId,
-      },
-      { status: 200 }
-    );
-    
+    const { file_key, file_name } = body;
+    console.log("****", file_key, file_name);
+    return NextResponse.json({ message: "success" });
   } catch (error) {
+    console.log(error);
     return NextResponse.json(
       {
         error: "internal server error",
